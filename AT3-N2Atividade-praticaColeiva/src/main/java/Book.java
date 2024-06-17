b0a920dccbcc32f2f76247cab0278d1c32c8c569
Victor_RTA <117090218+RoxasTA@users.noreply.github.com>
2024-06-16 23:00:16 -0300

    Add files via upload

---
+++ AT3-N2Atividade-praticaColeiva/src/main/java/Book.java
@@ -0,0 +1,58 @@
+public class Book {
+    private String author;
+    private String title;
+    private String genre;
+    private int samples;
+
+    public Book() {
+    }
+
+    public Book(String author, String title, String genre, int samples) {
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+        this.samples = samples;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getSamples() {
+        return samples;
+    }
+
+    public void setSamples(int samples) {
+        this.samples = samples;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", samples=" + samples +
+                '}';
+    }
+}
